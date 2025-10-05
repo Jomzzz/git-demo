7bd2eb634af1e5eb371be01e9ad10ae527892c03
Jom <842034010@qq.com>
2025-10-06 02:25:22 +0800

    first commit test

---
+++ test.java
@@ -0,0 +1,11 @@
+/**
+ *@title test
+ *@description <TODO description class purpose>
+ *@author 84203
+ *@version 1.0.0
+ *@create 2025/10/6 2:22
+ **/public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
